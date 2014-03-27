bd7c3e7b18571c22569f253b460d42f96d7e3f7d
Matt McManus <matt@mcmanus.io>
2014-03-26 22:40:58 -0400

    Bower upgrades and a homepage I like

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,18 +1,24 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
-$(document).foundation();
 
-var parallax = document.getElementsByClassName('background-image')[0];
-var foreground = $('#header .row')
 
-window.onscroll = function(e) {
-  if (window.scrollY < 500) {
-    // note: most browsers presently use prefixes: webkitTransform, mozTransform etc.
-    var translate3d = 'translate3d(0px,' + (window.scrollY/2) + 'px, 0px)';
-    parallax.style.webkitTransform = translate3d;
-    parallax.style.transform = translate3d;
-    foreground.each(function(x,f){
-      f.style.opacity = 1-(window.scrollY*.005);
-    })
+$(document).ready(function() {
+  $(document).foundation();
+
+  var parallax = document.getElementsByClassName('background-image')[0];
+  var foreground = $('#header .row')
+
+  window.onscroll = function(e) {
+    if (window.scrollY < 500) {
+      // note: most browsers presently use prefixes: webkitTransform, mozTransform etc.
+      var translate3d = 'translate3d(0px,' + (window.scrollY/1.5) + 'px, 0px)';
+      parallax.style.webkitTransform = translate3d;
+      parallax.style.transform = translate3d;
+      foreground.each(function(x,f){
+        f.style.opacity = 1-(window.scrollY*.0005);
+      })
+    }
   }
-}
+
+  $(".lettering p").lettering('words');
+});
