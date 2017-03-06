e6526f694cd848f9741fb4e48decd95ba720258d
Matt McManus <matt@mcmanus.io>
2017-03-05 21:10:15 -0500

    Update design on blog and article pages

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,29 +1,93 @@
-// Foundation JavaScript
-// Documentation can be found at: http://foundation.zurb.com/docs
+(function(window, document) {
+  'use strict';
 
+  var flip = 0;
 
-$(document).ready(function() {
+  function toggleGrid() {
+    var body = document.getElementsByTagName('body')[0];
+    var button = document.getElementById('btnToggleGrid');
 
-  var parallax = document.getElementsByClassName('background-image')[0];
-  var foreground = document.getElementsByClassName('background-image-foreground')[0];
-  var content = $('#header .row');
-
-  window.onscroll = function(e) {
-    if (window.scrollY < 700) {
-      // note: most browsers presently use prefixes: webkitTransform, mozTransform etc.
-      var translate3d = 'translate3d(0px,' + (window.scrollY/2) + 'px, 0px)';
-      parallax.style.webkitTransform = translate3d;
-      parallax.style.transform = translate3d;
-      var opacity = 1-(window.scrollY*0.0009);
-      if (opacity > 1) {
-        foreground.style.opacity = 1-((opacity-1)*20);
-      }
-
-      // content.each(function(x,f){
-      //   f.style.opacity = opacity;
-      // });
+    if (flip === 0) {
+      body.classList.add('grid');
+      button.textContent = 'Turn Double Grid On';
+      button.classList.add('double-grid');
+      flip = 1;
+    } else if (flip === 1) {
+      body.classList.add('grid-double');
+      button.textContent = 'Turn Grid Off';
+      button.classList.remove('double-grid');
+      button.classList.add('grid-off');
+      flip = 2;
+    } else if (flip === 2) {
+      body.classList.remove('grid');
+      body.classList.remove('grid-double');
+      button.classList.remove('grid-off');
+      button.textContent = 'Turn Grid On';
+      flip = 0;
     }
+  }
+
+  /* Fix image height to fit into the baseline grid
+     ========================================================================== */
+
+  // 2. THE JAVASCRIPT WAY
+  // Construct the function
+  function fixImgHeight() {
+
+    // Get all images
+    var images = document.querySelectorAll('img');
+
+    // Get line-height
+    var element = document.getElementsByTagName('body')[0];
+    var style = window.getComputedStyle(element);
+    var lineHeight = parseInt(style.getPropertyValue('line-height'));
+    var i;
+    var length = images.length;
+    var imgOriginalHeight;
+    var div;
+    var imgNewHeight;
+
+    // For each image in images get original height, calculate height rounded to baseline grid, set new height
+    for (i = 0; i < length; ++i) {
+
+      //Reset height first
+      images[i].style.height = 'auto';
+
+      // Get original height
+      imgOriginalHeight = images[i].offsetHeight;
+
+      // Calculate new height
+      div = Math.floor(imgOriginalHeight / lineHeight);
+      imgNewHeight = lineHeight * div;
+
+      // Set new height
+      images[i].style.height = imgNewHeight + 'px';
+    }
+  }
+
+  document.getElementById('btnToggleGrid').onclick = toggleGrid;
+
+  //Fix once on first page load
+  fixImgHeight();
+
+  //Make sure that we fix images on each window resize (add debounce for performance)
+  window.addEventListener('resize', debounce(fixImgHeight, 50), true);
+
+
+  //helper: debounce
+  function debounce(func, wait, immediate) {
+    var timeout;
+    return function() {
+      var context = this, args = arguments;
+      var later = function() {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+      };
+      var callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) func.apply(context, args);
+    };
   };
 
-  //$(".lettering p, .skills h3").lettering('words');
-});
+}(window, document));
