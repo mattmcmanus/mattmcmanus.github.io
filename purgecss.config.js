a72288b973cf6330afa98ee5c51a85c91b364a36
Matt McManus <matt@mcmanus.io>
2018-08-26 21:11:50 -0400

    Fix purgecss extraction for tailwind

--- purgecss.config.js
+++ purgecss.config.js
@@ -1,5 +1,17 @@
+class TailwindExtractor {
+  static extract(content) {
+    return content.match(/[A-z0-9-:\/]+/g);
+  }
+}
+
 module.exports = {
   content: ['_site/**/*.html'],
   css: ['_site/styles.css'],
-  whitelist: []
+  whitelist: [],
+  extractors: [
+    {
+      extractor: TailwindExtractor,
+      extensions: ['html']
+    }
+  ]
 }
