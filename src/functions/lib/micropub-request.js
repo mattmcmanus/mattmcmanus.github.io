08c8e344b3c99eac00cbdc365ff10926f9f980d0
Matt McManus <matt@mcmanus.io>
2018-09-01 09:18:45 -0400

    Basic micropub endpoint

---
+++ src/functions/lib/micropub-request.js
@@ -0,0 +1,42 @@
+import fetch from 'node-fetch';
+const TOKEN_ENDPOINT = 'https://tokens.indieauth.com/token';
+
+export default class MicropubRequest {
+  constructor({
+    path,
+    httpMethod,
+    queryStringParameters,
+    headers = {},
+    body,
+    isBase64Encoded
+  }) {
+    this.method = httpMethod;
+    this.queryString = queryStringParameters;
+    this.headers = headers;
+    this.body = body;
+  }
+
+  async verifyToken() {
+    let bearer = this.headers.authorization;
+
+    if (!bearer) {
+      throw new Error('No Bearer Token');
+    }
+
+    let res;
+    try {
+      res = await fetch(TOKEN_ENDPOINT, {
+        'Accept': 'application/json',
+        // 'Authorization': bearer
+      });
+    } catch (e) {
+      console.error(e)
+    } finally {
+
+    }
+  	let json = await res.json();
+  	console.log('TOKEN', json);
+
+    return json;
+  }
+}
